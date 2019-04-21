1ad9aabb3d8fca0e9f15ac487a445f806da0e80a
DaaGeney <diegoassia@gmail.com>
2019-04-21 18:05:14 -0500

    Primer commit

---
+++ api.php
@@ -0,0 +1,98 @@
+<?php 
+
+$conn = new mysqli("localhost", "root", "123!\"·QWE", "software");
+if ($conn->connect_error) {
+	die("Database connection established Failed..");
+} 
+$res = array('error' => false);
+
+
+
+$action = 'read';
+
+if (isset($_GET['action'])) {
+	$action = $_GET['action'];
+}
+
+if ($action == 'read') {
+	$result = $conn->query("SELECT * FROM 'usuarios'");
+	$users = array();
+
+	while ($row = $result->fetch_assoc()){
+		array_push($users, $row);
+	}
+	$res['users'] = $users;
+}
+
+if ($action == 'create') {
+
+	$id = $_POST['id'];
+	$nombre = $_POST['nombre'];
+	$apellido = $_POST['apellido'];
+	$correo = $_POST['correo'];
+	$clave = $_POST['clave'];
+	$credencial = $_POST['credencial'];
+	$idlista = $_POST['idlista'];
+	$idprograma = $_POST['idprograma'];
+
+
+	$result = $conn->query("INSERT INTO 'usuarios' ('id', 'nombre', 'apellido','correo','clave','credencial','idlista','idprograma') VALUES (	'$id','$nombre', '$apellido','$correo','$clave','$credencial','$idlista','$idprograma') ");
+	if ($result) {
+		$res['message'] = "User Added successfully";
+	} else{
+		$res['error'] = true;
+		$res['message'] = "Insert User fail";
+	}
+}
+
+
+if ($action == 'update') {
+	$id = $_POST['id'];
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$mobile = $_POST['mobile'];
+
+
+	$result = $conn->query("UPDATE `users` SET `username` = '$username', `email` = '$email', `mobile` = '$mobile'WHERE `id` = '$id'");
+	if ($result) {
+		$res['message'] = "User Updated successfully";
+	} else{
+		$res['error'] = true;
+		$res['message'] = "User Update failed";
+	}
+}
+
+
+
+
+
+if ($action == 'delete') {
+	$id = $_POST['id'];
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$mobile = $_POST['mobile'];
+
+
+	$result = $conn->query("DELETE FROM `users` WHERE `id` = '$id'");
+	if ($result) {
+		$res['message'] = "User deleted successfully";
+	} else{
+		$res['error'] = true;
+		$res['message'] = "User delete failed";
+	}
+}
+
+
+
+
+
+
+
+
+
+$conn -> close();
+header("Content-type: application/json");
+echo json_encode($res);
+die();
+
+ ?>
